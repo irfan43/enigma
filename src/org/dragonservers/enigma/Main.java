abe79c47000eb8d26b12a1c7f595849dadbb1577
irfan <irfan00roshan@gmail.com>
2021-03-01 00:01:44 +0530

    interface for File Saving of Key pair,

--- src/org/dragonservers/enigma/Main.java
+++ src/org/dragonservers/enigma/Main.java
@@ -2,27 +2,57 @@ package org.dragonservers.enigma;
 
 import javax.crypto.KeyAgreement;
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    private static KeyPairGenerator KeyPGen;
-
-
+    private static KeyPair Kpair = null;
+    private static PublicKey TherePubKey = null;
 
     public static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
+        while(true) {
+            System.out.println("Menu");
+            System.out.println("1 - Load or Generate new Key");
+            System.out.println("2 - Load There Pub Key");
+            System.out.println("3 - Create a Shared Secret");
+            System.out.println("4 - Display Keys Generated");
+            System.out.println("E - Exit");
+            String Response = scn.nextLine();
+            switch (Response) {
+                case "1":
+                    InitialiseKeys();
+                    break;
+                case "2":
+                    InitialiseThem();
+                    break;
+                case "3":
+                    GenerateSecret();
+                    break;
+                case "4":
+                    DisplayKey();
+                    break;
+                case "E":
+                case "e":
+                    return;
+            }
+        }
+        /*
         try {
 
             System.out.println("Creating A Key Pair");
-            KeyPGen = KeyPairGenerator.getInstance("DH");
+            KeyPairGenerator KeyPGen = KeyPairGenerator.getInstance("DH");
             KeyPGen.initialize(2048);
-            KeyPair Kpair = KeyPGen.generateKeyPair();
+            Kpair = KeyPGen.generateKeyPair();
+
             KeyAgreement KAgree = KeyAgreement.getInstance("DH");
             KAgree.init(Kpair.getPrivate());
 
@@ -32,7 +62,7 @@ public class Main {
             //System.out.println(toHexString(PubKeyEnc));
 
             System.out.println("Enter File Name for There Public Key:-");
-            byte[] ThPubKeyEnc = readPubkey(scn.nextLine() + ".pub");
+            byte[] ThPubKeyEnc = readkey(scn.nextLine() + ".pub");
 
             KeyFactory KFactory = KeyFactory.getInstance("DH");
             X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(ThPubKeyEnc);
@@ -44,21 +74,101 @@ public class Main {
             SaveKey(SharedScrt, scn.nextLine() + ".sct");
             System.out.println("Shared Secret:-" + toHexString(SharedScrt));
 
-        } catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException | InvalidKeyException | InvalidKeySpecException e) {
             e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
+        }*/
+    }
+
+    private static void DisplayKey() {
+        if(Kpair != null){
+            System.out.println("Our Public Key     :-\n" + toHexString(Kpair.getPublic().getEncoded()));
+            System.out.println("Our Private Key    :-\n" + toHexString(Kpair.getPrivate().getEncoded()));
+        }
+        if(TherePubKey != null)
+            System.out.println("There Public Key   :-\n" + toHexString(TherePubKey.getEncoded()));
+
+    }
+
+    private static void GenerateSecret() {
+    }
+
+    private static void InitialiseThem() {
+    }
+
+    private static void InitialiseKeys() {
+        String resp;
+        try {
+            System.out.println("(G)enerate Keys or Load From (F)ile? (G/F) [any to return to main menu]");
+            resp = scn.nextLine();
+            if(resp.equalsIgnoreCase("f")){
+                //load the Key pair from a file
+                String FileName = scn.nextLine();
+                Kpair = getKeyPair(FileName + ".kpr");
+            }else if(resp.equalsIgnoreCase("g")) {
+                //Generate new keys
+                KeyPairGenerator KPgen = KeyPairGenerator.getInstance("DH");
+                KPgen.initialize(2048);
+                Kpair = KPgen.generateKeyPair();
+                System.out.println("Enter File Name to save the KeyPair:-");
+                SaveKeyPair(scn.nextLine() + ".kpr",Kpair);
+            }
+        } catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
 
-    private static byte[] readPubkey(String FileName) {
+    private static void SaveKeyPair(String Filename,KeyPair kp) {
+        byte[] Pubenc = kp.getPublic().getEncoded(), Prvenc = kp.getPrivate().getEncoded();
+        byte[] FileBin = new byte[Pubenc.length + Prvenc.length + 8];
+
+        ByteBuffer PubBB = ByteBuffer.allocate(4);
+        PubBB.putInt(Pubenc.length);
+        byte[] PubLenIntEnc = PubBB.array();
+
+        ByteBuffer PrvBB = ByteBuffer.allocate(4);
+        PrvBB.putInt(Prvenc.length);
+        byte[] PrvLenIntEnc = PrvBB.array();
+
+        System.arraycopy(PubLenIntEnc, 0,FileBin,0,4);
+        System.arraycopy(Pubenc, 0, FileBin, 4,Pubenc.length);
+        System.arraycopy(PrvLenIntEnc, 0,FileBin,Pubenc.length + 4,4);
+        System.arraycopy(Prvenc,0, FileBin,Pubenc.length + 8,Prvenc.length);
+
+        System.out.println(" Public Len = " + Pubenc.length + "private len = " + Prvenc.length );
+        SaveKey(FileBin,Filename);
+
+    }
+
+    private static KeyPair getKeyPair(String FileName) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyPair kp;
+        byte[] PublicKeyEnc,PrivateKeyEnc;
+        byte[] FileBin = readkey(FileName);
+        int PubLen,PrvLen;
+
+        byte[] PubLenEnc = Arrays.copyOfRange(FileBin,0,4);
+        ByteBuffer PubBB = ByteBuffer.wrap(PubLenEnc);
+        PubLen = PubBB.getInt();
+        PublicKeyEnc = Arrays.copyOfRange(FileBin,4,PubLen + 4);
+
+        byte[] PrvLenEnc = Arrays.copyOfRange(FileBin,PubLen + 4,PubLen + 8);
+        ByteBuffer PrvBB = ByteBuffer.wrap(PrvLenEnc);
+        PrvLen = PrvBB.getInt();
+        PrivateKeyEnc = Arrays.copyOfRange(FileBin, PubLen + 8,PubLen + 8 + PrvLen);
+
+        KeyFactory PubKF = KeyFactory.getInstance("DH");
+        PublicKey pubk = PubKF.generatePublic(new X509EncodedKeySpec(PublicKeyEnc));
+
+        KeyFactory PrvKF = KeyFactory.getInstance("DH");
+        PrivateKey prvk = PrvKF.generatePrivate(new PKCS8EncodedKeySpec(PrivateKeyEnc));
+
+        kp = new KeyPair(pubk,prvk);
+        return kp;
+    }
+
+    private static byte[] readkey(String FileName) {
         byte[] KeyEnc = null;
         try {
             KeyEnc = Files.readAllBytes(Paths.get(FileName));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
